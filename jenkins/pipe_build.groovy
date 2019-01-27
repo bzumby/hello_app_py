df471dc0c311dc27c9f7457ef9c832cf9e2634ab
Yan <yanp@iMac-Sirena.lan>
2019-01-27 23:08:20 +0200

    pipeline is correct, though
    cannot push to docker hub for some reason

--- jenkins/pipe_build.groovy
+++ jenkins/pipe_build.groovy
@@ -3,41 +3,19 @@ node {
 			git url: 'https://github.com/bzumby/hello_app_py.git',
 				branch: "master"
 	    }
+
 		stage ('Just Check') {
-			sh 'echo $PATH'
-			sh 'whoami'
-			sh 'pwd'
-			sh 'which docker'
+			withEnv(["PATH+DOCKER=/usr/local/bin"]) {
+				sh 'echo $PATH'
+				sh 'whoami'
+				sh 'pwd'
+				sh 'which docker'
+			}
 		}
+
 		stage ('Docker Build') {
-			docker.withTool('local') {
-				def customImage = docker.build("hello_app_py:${env.BUILD_ID}")
-        }
-    	}
-		stage ('Just Check 2') {
-			sh 'echo $PATH'
-			sh 'whoami'
-			sh 'pwd'
-			sh 'which docker'
+			def image = docker.build("bzumby/hello_app_py:${env.BUILD_ID}")
+				withDockerRegistry([credentialsId: 'docker_hub_bz', url: 'https://index.docker.io']) {
+            	image.push() }
 		}
-}
-
-
-
-// pipeline {
-// 	agent { label 'docker_slave'
-// 	}
-// 	stages {
-// 		stage('Git Clone Source') {
-// 			steps {
-// 				git url: 'https://github.com/bzumby/hello_app_py.git',
-// 					branch: "master"
-// 			}
-// 		}
-// 		stage('Docker Build') {
-// 			steps {
-// 				sh 'docker build -t ${repo}/hello_app_py:${BUILD_NUMBER} -t ${repo}/hello_app_py:latest . '				
-// 			}
-// 		}
-// 	}
-// }
+	}
